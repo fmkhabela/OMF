1fa8c84aca1c14b2efe4230d4ac11c043ad85d38
fmkhabela <fmkhabela@gmail.com>
2021-11-19 22:05:22 +0200

    Update ValidateRepaymentAmount.java

--- src/pageobject/ValidateRepaymentAmount.java
+++ src/pageobject/ValidateRepaymentAmount.java
@@ -52,7 +52,7 @@ public class ValidateRepaymentAmount extends BaseClass{
 	@FindBy(xpath = "//*[@id=\'blt9c764616951e6d18\']/div/div[2]/div[2]/om-calculator-result/div[2]/h5/strong")
 	@CacheLookup
 	WebElement validateAmount;
-	
+	//Select 60 MONTHS from the “How long do you need to repay it?” dropdown and click CALCULATE.
 	public void ValidateRepaymentAmountPageObject() throws InterruptedException, IOException {
 		
 		JavascriptExecutor js = (JavascriptExecutor) driver;
